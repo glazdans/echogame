24c58822f674a02b66323317f2363126f1322b09
glazdans <g.lazdans@gmail.com>
2015-10-13 21:59:48 +0300

    Initial Commit

---
+++ core/src/com/glazdans/echo/Entity.java
@@ -0,0 +1,8 @@
+package com.glazdans.echo;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Entity {
+    Vector2 position;
+    Vector2 mouseDirection;
+}
---
+++ desktop/src/com/glazdans/echo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.glazdans.echo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.glazdans.echo.GdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxGame(), config);
+	}
+}
