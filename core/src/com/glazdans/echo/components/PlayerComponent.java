08d03145eac7f3692915b74ceb6b7bd12b1785b0
glazdans <g.lazdans@gmail.com>
2016-01-08 22:28:19 +0200

    Create PlayerComponent.java

---
+++ core/src/com/glazdans/echo/components/PlayerComponent.java
@@ -0,0 +1,9 @@
+package com.glazdans.echo.components;
+
+import com.artemis.Component;
+
+public class PlayerComponent extends Component{
+    public float maxSpeed;
+    public float speedGain;
+
+}
