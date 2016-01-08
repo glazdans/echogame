50e938e6a2cb5b739f411d25f989cf5fa5cd665f
glazdans <g.lazdans@gmail.com>
2016-01-08 22:27:37 +0200

    Create InputComponent.java

---
+++ core/src/com/glazdans/echo/components/InputComponent.java
@@ -0,0 +1,11 @@
+package com.glazdans.echo.components;
+
+import com.artemis.Component;
+
+public class InputComponent extends Component {
+    public boolean pressUp;
+    public boolean pressDown;
+    public boolean pressLeft;
+    public boolean pressRight;
+    public boolean pressShoot;
+}
