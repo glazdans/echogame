3db830e9fc22cd3ee1002a420b89f943d7ce5dc7
glazdans <g.lazdans@gmail.com>
2016-01-08 22:27:51 +0200

    Create MovementComponent.java

---
+++ core/src/com/glazdans/echo/components/MovementComponent.java
@@ -0,0 +1,14 @@
+package com.glazdans.echo.components;
+
+import com.artemis.Component;
+import com.badlogic.gdx.math.Vector3;
+
+public class MovementComponent extends Component {
+    public Vector3 velocity;
+    public Vector3 acceleration;
+
+    public MovementComponent(){
+        velocity = new Vector3();
+        acceleration = new Vector3();
+    }
+}
